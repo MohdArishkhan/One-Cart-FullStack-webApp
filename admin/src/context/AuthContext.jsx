fcc7f1a60adad2c06e9ae9388800f4e4af476606
MohdArishkhan <writetoarishadgitm@gmail.com>
2025-08-06 14:28:42 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from 'react'
 
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://one-cart-1cvg.onrender.com"
 
     let value = {
       serverUrl
