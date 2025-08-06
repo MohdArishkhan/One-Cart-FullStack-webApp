a34fc9d81fef614d566120139d81020958f40d10
MohdArishkhan <writetoarishadgitm@gmail.com>
2025-08-06 15:16:50 +0530

    Update Firebase.js

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,8 +2,8 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
+  authDomain: "onecart--login.firebaseapp.com",
+  projectId: "onecart--login",
   storageBucket: "loginonecart.firebasestorage.app",
   messagingSenderId: "242165258894",
   appId: "1:242165258894:web:0155a2ced93e20073247df"
