4379371ff205e53829d283ef4816bdac74a99a1f
Mohd Arish Khan <writetoarish7@gmail.com>
2025-09-19 15:51:19 +0530

    readme updated

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://one-cart-3.onrender.com" , "https://one-cart-5.onrender.com","http://localhost:5173"],
  credentials:true
 }))
 
